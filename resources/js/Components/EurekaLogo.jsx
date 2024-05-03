220bc744a5fd48ff6127d54aa78a2d2e48d24b89
Danny Suggi Saputra <dannysaputra3003@gmail.com>
2024-05-03 10:25:51 +0700

    first commit

---
+++ resources/js/Components/EurekaLogo.jsx
@@ -0,0 +1,66 @@
+export default function EurekaLogo(props) {
+    return (
+        <svg
+            width="206"
+            height="47"
+            viewBox="0 0 206 47"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <g filter="url(#filter0_d_124_9)">
+                <path
+                    d="M4.1527 39V0.499997H23.8977V7.1H11.6877V16.285H22.5777V22.665H11.6877V32.62H24.1177V39H4.1527ZM43.3913 32.62H48.0113C48.818 31.74 49.3863 30.86 49.7163 29.98C50.083 29.0633 50.2663 27.725 50.2663 25.965V0.499997H57.8013V23.215C57.8013 25.085 57.7463 26.5517 57.6363 27.615C57.563 28.6417 57.343 29.9067 56.9763 31.41C56.353 34.0867 54.6297 36.6167 51.8063 39H39.5963C36.773 36.69 35.0497 34.16 34.4263 31.41C33.8763 29.1733 33.6013 26.4417 33.6013 23.215V0.499997H41.1363V25.965C41.1363 27.725 41.3013 29.0633 41.6313 29.98C41.998 30.86 42.5847 31.74 43.3913 32.62ZM68.3265 0.499997H82.9565C85.2665 1.27 87.2282 2.7 88.8415 4.79C90.4915 6.84333 91.3165 9.37333 91.3165 12.38C91.3165 17.2933 89.3732 20.905 85.4865 23.215L91.0415 39H83.3415L78.6665 24.975H75.7515V39H68.3265V0.499997ZM75.7515 7.1V18.815H80.4815C81.1049 18.6683 81.7832 18.045 82.5165 16.945C83.2865 15.8083 83.6715 14.4883 83.6715 12.985C83.6715 11.4817 83.3415 10.1617 82.6815 9.025C82.0582 7.88833 81.4349 7.24666 80.8115 7.1H75.7515ZM100.789 39V0.499997H120.534V7.1H108.324V16.285H119.214V22.665H108.324V32.62H120.754V39H100.789ZM130.513 39V0.499997H138.048V16.725H138.323L145.198 0.499997H152.953L144.923 18.87L153.338 39H144.978L138.433 22.17H138.048V39H130.513ZM177.862 39L176.707 32.895H168.017L166.862 39H159.382L166.807 0.499997H178.192L185.232 39H177.862ZM172.252 7.65L170.107 20.465L168.897 27.01H175.772L174.507 20.465L172.362 7.65H172.252ZM194.32 31.575H200.48V39H194.32V31.575ZM201.305 0.499997L200.48 18.1V25.745H194.32V18.1L193.495 0.499997H201.305Z"
+                    fill="url(#paint0_angular_124_9)"
+                />
+            </g>
+            <defs>
+                <filter
+                    id="filter0_d_124_9"
+                    x="0.152832"
+                    y="0.5"
+                    width="205.152"
+                    height="46.5"
+                    filterUnits="userSpaceOnUse"
+                    color-interpolation-filters="sRGB"
+                >
+                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                    <feColorMatrix
+                        in="SourceAlpha"
+                        type="matrix"
+                        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                        result="hardAlpha"
+                    />
+                    <feOffset dy="4" />
+                    <feGaussianBlur stdDeviation="2" />
+                    <feComposite in2="hardAlpha" operator="out" />
+                    <feColorMatrix
+                        type="matrix"
+                        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+                    />
+                    <feBlend
+                        mode="normal"
+                        in2="BackgroundImageFix"
+                        result="effect1_dropShadow_124_9"
+                    />
+                    <feBlend
+                        mode="normal"
+                        in="SourceGraphic"
+                        in2="effect1_dropShadow_124_9"
+                        result="shape"
+                    />
+                </filter>
+                <radialGradient
+                    id="paint0_angular_124_9"
+                    cx="0"
+                    cy="0"
+                    r="1"
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(13.7736 -9.46018) rotate(20.2792) scale(190.883 64.2046)"
+                >
+                    <stop stop-color="#02AF91" />
+                    <stop offset="1" stop-color="#0F0A02" />
+                </radialGradient>
+            </defs>
+        </svg>
+    );
+}
