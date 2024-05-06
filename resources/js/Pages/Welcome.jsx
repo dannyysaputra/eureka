413c888ca313d1d3744418f1be10d89c0db07309
denis <denisfirmansyah55@gmail.com>
2024-05-06 14:02:51 +0700

    feat: landing page finished

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,3 +1,4 @@
+import EurekaLogo from "@/Components/EurekaLogo";
 import MainLayout from "@/Layouts/MainLayout";
 import { Head } from "@inertiajs/react";
 import { TERipple } from "tw-elements-react";
@@ -18,12 +19,15 @@ export default function Welcome({ auth, photoPath }) {
                             <div className="relative mb-4 flex w-96 flex-wrap items-stretch">
                                 <input
                                     type="search"
-                                    className="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto bg-slate-200 rounded-lg border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-secondary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-secondary"
+                                    className="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto bg-slate-200 rounded-l-lg border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-secondary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-secondary"
                                     placeholder="Cari pertanyaan..."
                                     aria-label="Search"
                                     aria-describedby="button-addon1"
                                 />
-                                <i class="fa-solid fa-magnifying-glass"></i>
+                                <div className="rounded-r-lg bg-gray-600 p-2">
+                                    <i class="fa-solid fa-magnifying-glass mx-4 my-auto bg-gray-600"></i>
+                                </div>
+                                
                             </div>
                             <div>
                                 Ajukan pertanyaan mu
@@ -49,12 +53,37 @@ export default function Welcome({ auth, photoPath }) {
                 </div>   
             </div>
             
-            <div className="flex justify-left p-4 h-44 w-1/2 rounded-br-lg flex-wrap mx-10" style={{backgroundColor : "#02AF91"}}>       
-                    <div className="font-bold text-6xl text-center flex-auto">APA ITU EUREKA?</div>
+            <div className="flex justify-left p-10 h-44-auto w-1/2 rounded-br-lg flex-wrap my-10" style={{backgroundColor : "#02AF91"}}>       
+                    <div className="font-bold text-6xl text-center flex-auto text-white">APA ITU EUREKA?</div>
             </div>
 
-            <div className="justify-items-end p-4 h-44 w-1/2 rounded-br-lg flex-wrap mx-10" style={{backgroundColor : "#02AF91"}}>       
-                    <div className="font-bold text-xl text-center flex-auto">Nama Eureka! terinspirasi dari kata Yunani yang berarti “Saya menemukannya!” dan menunjukkan bahwa website ini dapat membantu user menemukan solusi untuk tugas SAINTEK.</div>
+            <div className="max-w-7xl mx-auto my-10 grid grid-cols-3 sm:px-6 lg:px-8">
+                <div className="flex-row size-4/5 mx-32">
+                    <img
+                        className=""
+                        src="/images/two-person.png"
+                        alt="Two-Person"
+                    />
+                </div>
+
+                <div className="absolute right-0 p-10 h-44-auto w-1/2 rounded-bl-lg flex-wrap my-32" style={{backgroundColor : "#02AF91"}}>
+                        <div className="font-bold text-xl text-center text-white">Nama Eureka! terinspirasi dari kata Yunani yang berarti “Saya menemukannya!” dan menunjukkan bahwa website ini dapat membantu user menemukan solusi untuk tugas SAINTEK.</div> 
+                </div>
+            </div>
+            
+            <div className="h-auto" style={{backgroundColor : "#02AF91"}}>
+                <div className="size-40 absolute right-0 h-auto mt-8">
+                    <img src="/images/saintek.png" alt="" />
+                </div>
+                <div className="flex justify-between items-center lg:px-8 lg:pt-8">
+                    <EurekaLogo></EurekaLogo> 
+                </div>
+                
+
+                <div className="mt-2 px-9 font-bold text-2xl text-white">
+                    <p>JADILAH YANG<br />PALING CERDAS</p>
+                </div>
+                <div className="font-light text-center">Copyright reserved</div>
             </div>
         </MainLayout>
     );
