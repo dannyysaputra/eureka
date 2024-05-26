fb253a2d19896f07e032b5a0914b7af039b2bf65
Danny Suggi Saputra <dannysaputra3003@gmail.com>
2024-05-26 12:06:19 +0700

    feat: view leaderboard

--- resources/js/Pages/Leaderboard.jsx
+++ resources/js/Pages/Leaderboard.jsx
@@ -3,10 +3,13 @@ import ArrowButton from "@/Components/ArrowButton";
 import Medal from "@/Components/Medal";
 import { Head, Link } from "@inertiajs/react";
 
-export default function DetailQuestion({ auth, photoPath }) {
+export default function DetailQuestion({ auth, photoPath, users }) {
+    console.log(auth.user);
+    const user = auth.user;
+
     return (
         <DetailQuestionLayout
-            user={auth.user}
+            user={user}
             header={
                 <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                     Leaderboard
@@ -25,36 +28,61 @@ export default function DetailQuestion({ auth, photoPath }) {
                     <h1 className="font-black text-4xl">Leaderboard</h1>
                 </div>
             </div>
-            <div
-                className="rounded-lg mx-12 my-8 p-7"
-                style={{ backgroundColor: "#02AF91" }}
-            >
-                <div className="flex justify-between">
-                    <div className="flex">
-                        <div className="me-5">
-                            <Medal fillColor={"yellow"} size={"64"}></Medal>
-                        </div>
 
-                        <div>
-                            <div className="flex mb-2">
-                                <div className="font-black text-xl">
-                                    Danny Suggi Saputra
+            {users.map((user, index) => {
+                let medalColor = "";
+                let rankDisplay = null;
+
+                if (index === 0) {
+                    medalColor = "#Ffd700";
+                    rankDisplay = <Medal fillColor={medalColor} size={"64"} />;
+                } else if (index === 1) {
+                    medalColor = "#C0C0C0";
+                    rankDisplay = <Medal fillColor={medalColor} size={"64"} />;
+                } else if (index === 2) {
+                    medalColor = "#CD7F32";
+                    rankDisplay = <Medal fillColor={medalColor} size={"64"} />;
+                } else if (index < 10) {
+                    rankDisplay = (
+                        <div className="rounded-full border-4 p-4">
+                            <div className="font-semibold text-2xl mx-2">
+                                {index + 1}
+                            </div>
+                        </div>
+                    );
+                }
+
+                return (
+                    <div
+                        key={user.id}
+                        className="rounded-lg mx-12 my-8 p-7"
+                        style={{ backgroundColor: "#02AF91" }}
+                    >
+                        <div className="flex justify-between">
+                            <div className="flex">
+                                <div className="me-5">{rankDisplay}</div>
+                                <div>
+                                    <div className="flex mb-2">
+                                        <div className="font-black text-xl">
+                                            {user.name}
+                                        </div>
+                                    </div>
+                                    <div className="font-semibold text-lg">
+                                        {user.jurusan.nama_jurusan}
+                                    </div>
                                 </div>
                             </div>
-                            <div className="font-semibold text-lg">
-                                Teknik Informatika
+                            <div className="flex">
+                                <div className="font-semibold">
+                                    {user.level}
+                                </div>
+                                <div className="mx-3"> - </div>
+                                <div>{user.points} pts</div>
                             </div>
                         </div>
                     </div>
-                    <div className="flex">
-                        <div className="font-semibold">
-                            Pemula
-                        </div>
-                        <div className="mx-3"> - </div>
-                        <div>500 pts</div>
-                    </div>
-                </div>
-            </div>
+                );
+            })}
         </DetailQuestionLayout>
     );
 }
